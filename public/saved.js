96088806313433625399dbd1449238b06872e475
Rich <rchrdholme@gmail.com>
2018-05-29 13:15:09 -0400

    modal add on saved page

--- public/saved.js
+++ public/saved.js
@@ -4,7 +4,11 @@ $( document ).ready(function() {
         // $("#saved").empty();
         $("#saved").empty();
         for (var i = 0; i < data.length; i++) {
-          $("#saved").append("<ul id='articleList'><li id='title' class='list-group-item active' data-id='" + data[i]._id + "'>" + data[i].title + "<button id='noteBtn' class='btn btn-success'>Add Note</button></li><li id='link' class='list-group-item'>" + data[i].link + "</li></ul>");
+          $("#saved").append("<ul id='articleList'><li id='title' class='list-group-item active' data-id='" + data[i]._id + "'>" + data[i].title + "<button id='deleteBtn' class='btn btn-danger'>Delete Fr`om Saved</button><button id='noteBtn' class='btn btn-success'>Add Note</button></li><li id='link' class='list-group-item'>" + data[i].link + "</li></ul>");
         }
-  });
+  	});
+
+    $(document).on("click", "#noteBtn", function() {
+  	  $(".modal").show();
+	});
 });
